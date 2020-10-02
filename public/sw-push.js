99b3b6d19370bfc3e8f13f53b2443566cca73f71
minhthao56 <minthao5648@gmail.com>
2020-10-02 07:53:30 +0700

    sw-push

---
+++ public/sw-push.js
@@ -0,0 +1,98 @@
+/*
+=======================================
+
+Public Key:
+BLHxWiNVmr7ROB8O3KpPRJFAMhMypwe4X9TdWMmhsPSzHszo32PDkndpvWx3H0OY2HwFCQRU98JBpZ_AEsVxWG4
+
+Private Key:
+VXcSZD3mdKyXEmDZNrB02WTxgUZmdIpAEo5tnXR4OH4
+
+=======================================
+
+*/
+
+const publicVapidKey =
+  "BOo3F7CV18dk-hxAIk0Q59qRkVu0o_4MQNoLP7pLgDPXaUloBfOqnSqXBsEFCmW2H059TJABMbviIR7vkh6hORw";
+
+const urlB64ToUint8Array = (base64String) => {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
+// saveSubscription saves the subscription to the backend
+const saveSubscription = async (subscription) => {
+  console.log("subscription");
+  console.log(subscription);
+  // const SERVER_URL = "http://localhost:3000/save-subscription";
+  // const response = await fetch(SERVER_URL, {
+  //   method: "post",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(subscription),
+  // });
+  // return response.json();
+};
+
+self.addEventListener("activate", async () => {
+  // This will be called only once when the service worker is activated.
+  console.log("Hello from service worker.");
+
+  try {
+    const applicationServerKey = urlB64ToUint8Array(publicVapidKey);
+
+    const options = { applicationServerKey, userVisibleOnly: true };
+
+    const subscription = await self.registration.pushManager.subscribe(options);
+    console.log("subs: ", JSON.stringify(subscription));
+
+    const response = await saveSubscription(subscription);
+    console.log(response);
+
+    /* LOG OUTPUT: Google
+        subs:  {"endpoint":"https://fcm.googleapis.com/fcm/send/c3DsU5n5NOA:APA91bGsQ_RdhGdQX7T2kipw-vILovDZxJlOD4SjlFAWVg2WAf2gTwqbJAnOgJhofW_W7aDFvUaJzrxYOdZUd1XnJZ6d3LO79_4ILUaiNEYmkb0gaCY-NkvU_x2-6_1xIp4bVf-Fg6Ll",
+        "expirationTime":null,
+        "keys":{"p256dh":"BOkas3eN-2DPbeY8RvR2g7NVOEcsmt5cOYTOSR-rihQKpPJWdODW4TFwk65vegkPDHue1iYbAe8W9mCzHWD67a4","auth":"jFL-bwVPRA_SnMoRIq-V7A"}}
+    */
+
+    /* LOG OUTPUT: Firefox
+        subs:  {"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABdBgQO2MKPusxFsBeY5DU7XG-dJ1udXvfWoyK7PIcSkeP2p_vBwRBk8RQbWi2xl7h8rkqryqxnUOon_GoOKCfJmC78nptCE60iqryTfpH_5USoLp1GND-ySxE_eBEfOsXaSe6yReOLrQszBo2VHjv6NezaqmwDhLCbnzz7gn_ikQ_UqhA",
+        "keys":{"auth":"D3VDFb4jT4Bqn-iH8VzxJw","p256dh":"BE3P_2G0x2aJTmyuUysOrov1_PDJyZ603hraYOZEonCBAdGU5Wi1POHc6_Geseooawzig2zjGNfl6zkIamYEmxs"}} 
+
+    */
+  } catch (err) {
+    console.log("Error", err);
+  }
+});
+
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    console.log("Push event!! ", event.data.text());
+  } else {
+    console.log("Push event but no data");
+  }
+});
+
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    console.log("Push event!! ", event.data.text());
+    showLocalNotification("Hurray !", event.data.text(), self.registration);
+  } else {
+    console.log("Push event but no data");
+  }
+});
+const showLocalNotification = (title, body, swRegistration) => {
+  const options = {
+    body,
+    // here you can add more properties like icon, image, vibrate, etc.
+  };
+  swRegistration.showNotification(title, options);
+};
--- src/index.js
+++ src/index.js
@@ -23,3 +23,63 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+const check = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No Service Worker support!");
+  }
+  if (!("PushManager" in window)) {
+    throw new Error("No Push API Support!");
+  }
+};
+
+const registerServiceWorker = async () => {
+  const swUrl = `${process.env.PUBLIC_URL}/sw-push.js`;
+  console.log("swUrl", swUrl);
+
+  const swRegistration = await navigator.serviceWorker.register(swUrl, {
+    scope: "/worker/",
+  });
+  return swRegistration;
+};
+
+const requestNotificationPermission = async () => {
+  //const permission = await window.Notification.requestPermission();
+
+  Notification.requestPermission((status) => {
+    console.log("Notification permission status:", status);
+  });
+
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+
+  // if (permission !== "granted") {
+  //   throw new Error("Permission not granted for Notification");
+  // }
+};
+
+const main = async () => {
+  check();
+  const swRegistration = await registerServiceWorker();
+  await requestNotificationPermission();
+
+  console.log("swReg", swRegistration);
+
+  if (Notification.permission == "granted") {
+    navigator.serviceWorker.getRegistration("/worker/").then((reg) => {
+      console.log("About to show notification", reg);
+
+      // reg.showNotification("Hello world!");
+    });
+
+    // navigator.serviceWorker.ready.then(function(reg) {
+    //   new Notification("Helo");
+    // });
+  }
+};
+
+main();
